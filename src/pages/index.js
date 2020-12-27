aef5fb9458ddcd80c038884ca6a369aa08ddd690
selindayioglu <63448943+selindayioglu@users.noreply.github.com>
2020-12-27 23:14:30 +0300

    fotoğraf ekleme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,31 @@
 import React from "react"
-const IndexPage = () => (
-  <div>
-    <h1>Merhaba</h1>
-  </div>
-)
-
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./style.css"
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div className="photo">
+      {data.allFile.edges.map(({ node }) => {
+        return <Img key={node.id} fluid={node.childImageSharp.fluid} />
+      })}
+    </div>
+  )
+}
+export const pageQuery = graphql`
+  {
+    allFile(filter: { absolutePath: { regex: "//photo//" } }) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
